975955c05311e82daea814c5729cf60460fafac0
Aaron <NewblyAaron@users.noreply.github.com>
2019-11-05 19:47:02 +0800

    Add project files.

---
+++ Decrypter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Decrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CryptTypeBox = new System.Windows.Forms.ComboBox();
+            this.OutputBox = new System.Windows.Forms.RichTextBox();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.Location = new System.Drawing.Point(57, 57);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(237, 20);
+            this.InputBox.TabIndex = 0;
+            this.InputBox.TextChanged += new System.EventHandler(this.InputBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Crypter";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output";
+            // 
+            // CryptTypeBox
+            // 
+            this.CryptTypeBox.FormattingEnabled = true;
+            this.CryptTypeBox.Items.AddRange(new object[] {
+            "En",
+            "De"});
+            this.CryptTypeBox.Location = new System.Drawing.Point(12, 12);
+            this.CryptTypeBox.Name = "CryptTypeBox";
+            this.CryptTypeBox.Size = new System.Drawing.Size(47, 21);
+            this.CryptTypeBox.TabIndex = 3;
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Enabled = false;
+            this.OutputBox.Location = new System.Drawing.Point(57, 88);
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(237, 96);
+            this.OutputBox.TabIndex = 4;
+            this.OutputBox.Text = "";
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(219, 190);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(75, 23);
+            this.CopyButton.TabIndex = 5;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 224);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.CryptTypeBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Crypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox CryptTypeBox;
+        private System.Windows.Forms.RichTextBox OutputBox;
+        private System.Windows.Forms.Button CopyButton;
+    }
+}
+
---
+++ Decrypter/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decrypter
+{
+    public partial class Form1 : Form
+    {
+        public char[] Crypt =
+        {
+            'E',
+            'G',
+            'Q',
+            'S',
+            'O',
+            'P',
+            'B',
+            'X',
+            'V',
+            'K',
+            'Y',
+            'U',
+            'Z',
+            'J',
+            'M',
+            'H',
+            'N',
+            'C',
+            'W',
+            'F',
+            'L',
+            'A',
+            'R',
+            'D',
+            'T',
+            'I'
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            CryptTypeBox.SelectedIndex = 0;
+        }
+
+        private void InputBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!(CryptTypeBox.SelectedIndex < 0))
+            {
+                if (CryptTypeBox.SelectedIndex == 0)
+                {
+                    Encrypt();
+                }
+                else if (CryptTypeBox.SelectedIndex == 1)
+                {
+                    Decrypt();
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(OutputBox.Text);
+        }
+
+        private void Decrypt()
+        {
+            string[] input = InputBox.Text.Split();
+            string output = "";
+
+            foreach (string s in input)
+            {
+                if (s == "-")
+                {
+                    output += " ";
+                    continue;
+                }
+
+                if (int.TryParse(s, out int index))
+                {
+                    output += GetCharFromIndex(--index);
+                }
+            }
+
+            OutputBox.Text = output;
+        }
+
+        private void Encrypt()
+        {
+            if (string.IsNullOrWhiteSpace(InputBox.Text))
+            {
+                InputBox.Clear();
+                return;
+            }
+
+            string[] input = InputBox.Text.Split();
+            string output = "";
+
+            foreach (string s in input)
+            {
+                char[] chars = s.ToCharArray();
+                int charLength = chars.Length;
+                int iteration = 1;
+
+                foreach (char c in chars)
+                {
+                    int index = GetIndexFromChar(c);
+
+                    if (index != -1)
+                    {
+                        output += index + " ";
+                    }
+
+                    if (iteration == charLength)
+                    {
+                        output += "- ";
+                    }
+
+                    iteration++;
+                }
+            }
+
+            OutputBox.Text = output.Remove(output.Length - 2);
+        }
+
+        private string GetCharFromIndex(int index)
+        {
+            foreach (char cryptchar in Crypt)
+            {
+                if (index == Array.IndexOf(Crypt, cryptchar))
+                {
+                    return cryptchar.ToString();
+                }
+            }
+
+            return null;
+        }
+
+        private int GetIndexFromChar(char c)
+        {
+            foreach (char cryptchar in Crypt)
+            {
+                if (c == cryptchar)
+                {
+                    return Array.IndexOf(Crypt, cryptchar) + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
